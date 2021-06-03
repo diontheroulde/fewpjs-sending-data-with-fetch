b25dd68e9d67a14b928b2a8a0c0caf19a1854fac
Dion Theroulde <dionjr105@gmail.com>
2021-06-03 16:17:59 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,83 @@
-// Add your code here
+
+    function submitData(name, email){
+        
+        const userData = {
+            name : name,
+            email : email
+        }
+        
+        const configurationObject = {
+            method : "POST",
+            headers : {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body : JSON.stringify(userData)
+
+            
+        }
+        
+        
+        
+        return fetch('http://localhost:3000/users', configurationObject)
+                .then(response => response.json())
+                .then(object => {  // document.body.innerHTML = object[ "id" ]
+                    console.log(object)
+                    const newID = document.createElement('body')
+                    newID.innerHTML =  object.id
+                    document.querySelector("body").append(newID)  
+                })
+                .catch(error => {  //document.body.innerHTML = error.message
+                    console.log(error)
+                    alert("Uh Oh!")
+                    const objectError = document.createElement('body')
+                    objectError.innerHTML = error.message
+                    document.querySelector('body').append(objectError)  
+
+                    
+                    
+
+                })
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const formData =  {
+//     dogName: "Byron",
+//     dogBreed :"Poodle"
+// }
+
+
+// const configurationObject = {
+//         method : "POST",
+//         headers : {
+//             "Content-Type": "application/json",
+//             "Accept": "application/json"
+//         },
+//         body : JSON.stringify(formData)
+//     }
+
+// fetch('http://localhost:3000/dogs', configurationObject)
+//     .then(response => response.json()) 
+//     .then(object => console.log(object))
